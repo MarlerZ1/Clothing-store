b132d169370313d88703b00dbd8646a6d272bc4b
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-03 15:06:12 +0300

    add new app "products"

--- store_server/settings.py
+++ store_server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'store_server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'products/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
