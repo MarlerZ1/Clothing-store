78b616e6c1bfd9b1a613628a28ee2fe258d79c71
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-04 15:35:55 +0300

    Add base template

--- store_server/urls.py
+++ store_server/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from products.views import index,products
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name=''),
+    path('', index, name='index'),
     path('products', products, name='products')
 ]
