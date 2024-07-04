ac685175da03d0724ecf27746c2af8f2a7f10999
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-04 16:46:05 +0300

    Add models, rework product`s view
    Add Product, ProductCategory models
    Rework prodyct`s view context

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from products.models import Product, ProductCategory
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity']
+    fields = ['name', 'price', 'quantity']
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+    fields = ['name',]
