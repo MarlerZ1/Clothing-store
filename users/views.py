e0d82bf2ac2f2e53e94a0625bb5dbc6b40c159bf
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-05 17:26:11 +0300

    Add profile page
    Add profile page with form

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.contrib import auth
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 # Create your views here.
@@ -39,3 +39,23 @@ def registration(request):
 
     context = {'form': form}
     return render(request, 'users/register.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:profile"))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+
+    context = {
+        'title': 'Store - Профиль',
+        'form': form
+    }
+
+    return render(request, 'users/profile.html', context)
