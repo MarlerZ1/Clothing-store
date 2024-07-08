a2e6bec2c38bb8a56f135834138654a5ab5066d1
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-08 12:24:15 +0300

    Change DB to postgresql

--- store_server/settings.py
+++ store_server/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'store_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('POSTGRES_ENGINE'),
+        'NAME': os.getenv('POSTGRES_NAME'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
     }
 }
 
