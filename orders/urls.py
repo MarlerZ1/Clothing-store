f75125829d13c89777b484bd2261a02acf81dcc1
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-13 14:02:02 +0300

    Add order`s detail page

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from orders.views import OrderCreateView, CancelTemplateView, SuccessTemplateView, OrderListView
+from orders.views import OrderCreateView, CancelTemplateView, SuccessTemplateView, OrderListView, OrderDetailView
 
 app_name = 'orders'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('order-create', OrderCreateView.as_view(), name="order_create"),
     path('order-success', SuccessTemplateView.as_view(), name="order_success"),
     path('order-canceled', CancelTemplateView.as_view(), name="order_canceled"),
+    path('order/<int:pk>', OrderDetailView.as_view(), name="order"),
     path('', OrderListView.as_view(), name="orders_list"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,17 +1,14 @@
 from http import HTTPStatus
 
+import stripe
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import CreateView, TemplateView, ListView
+from django.views.generic import CreateView, TemplateView, ListView, DetailView
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
-
-import stripe
-
 from orders.models import Order
 from products.models import Basket
 from store_server import settings
@@ -30,7 +27,7 @@ class OrderCreateView(LoginRequiredMixin, TitleMixin, CreateView):
         last_order = Order.objects.filter(initiator=self.request.user).last()
         if last_order and last_order.status == 0:
             last_order.delete()
-            
+
         super(OrderCreateView, self).post(self, request, *args, **kwargs)
         baskets = Basket.objects.filter(user=self.request.user)
 
@@ -53,9 +50,21 @@ class OrderListView(TitleMixin, ListView):
     title = 'Store - Заказы'
     model = Order
     ordering = ('-created',)
+
     def get_queryset(self):
-        queryset = super(OrderListView,self).get_queryset()
+        queryset = super(OrderListView, self).get_queryset()
         return queryset.filter(initiator=self.request.user)
+
+
+class OrderDetailView(DetailView):
+    template_name = 'orders/order.html'
+    model = Order
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderDetailView, self).get_context_data(**kwargs)
+        context['title'] = f'Store - Заказ #{self.object.id}'
+        return context
+
 
 class SuccessTemplateView(LoginRequiredMixin, TitleMixin, TemplateView):
     template_name = 'orders/success.html'
@@ -66,30 +75,31 @@ class CancelTemplateView(LoginRequiredMixin, TitleMixin, TemplateView):
     template_name = 'orders/canceled.html'
     title = 'Store - Заказ отменен'
 
+
 @csrf_exempt
 def stripe_webhook_view(request):
-  payload = request.body
-  sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-  event = None
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
 
-  try:
-    event = stripe.Webhook.construct_event(
-      payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
-    )
-  except ValueError as e:
-    # Invalid payload
-    return HttpResponse(status=400)
-  except stripe.error.SignatureVerificationError as e:
-    # Invalid signature
-    return HttpResponse(status=400)
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
 
-  if (
-    event['type'] == 'checkout.session.completed'
-    or event['type'] == 'checkout.session.async_payment_succeeded'
-  ):
-    fulfill_checkout(event['data']['object'])
+    if (
+            event['type'] == 'checkout.session.completed'
+            or event['type'] == 'checkout.session.async_payment_succeeded'
+    ):
+        fulfill_checkout(event['data']['object'])
 
-  return HttpResponse(status=200)
+    return HttpResponse(status=200)
 
 
 def fulfill_checkout(session):
